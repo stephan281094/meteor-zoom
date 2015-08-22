e69ba076420104db46bf9e701b9a22ed587bddeb
stephan281094 <stephan281094@gmail.com>
2015-08-22 16:57:18 +0200

    Change username because my previous username is way too complicated :P

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'stephan281094:zoom',
+  name: 'inno:zoom',
   version: '0.0.1',
   summary: 'Wrapper for Medium\'s Image Zoom Plugin',
   git: 'https://github.com/stephan281094/meteor-zoom'
